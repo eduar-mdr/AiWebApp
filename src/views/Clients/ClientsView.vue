f52fed3ad710ca6de99b5f26a32a8d30b9bbce89
Eduardo Medrano <87587568+eduar-mdr@users.noreply.github.com>
2024-06-11 00:18:20 -0600

    ClientsView: updates

--- src/views/Clients/ClientsView.vue
+++ src/views/Clients/ClientsView.vue
@@ -2,11 +2,58 @@
 <v-container :fluid=true>
     <h1 class="text-grey-darken-1">Clientes</h1>
     <h2 class="text-grey-lighten-1">Contenido</h2>
+    <v-table density="compact" v-show="data">
+    <thead>
+      <tr>
+        <th class="text-left">
+          Nombre
+        </th>
+        <th class="text-left">
+          Correo
+        </th>
+        <th class="text-left">
+          Intereses
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in data"
+        :key="item?.id"
+      >
+        <td>{{ item?.content?.name }} {{ item?.content?.last_name }}</td>
+        <td>{{ item?.content?.email }} </td>
+        <td >
+            <v-chip variant="flat"  size="small" v-for="interest in item?.content?.interests" :key="interest"  class="text-center">
+            {{ interest }}
+            </v-chip>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </v-container>
 </template>
 
 <script>
+
 export default {
-name: 'ClientsView'
+    name: 'ClientsView',
+    data(){
+        return{
+            data:null
+        }
+    },
+    methods: {
+        getClients(){
+            this.data = this.$store.getters.getContextClients
+            console.log(this.data);
+        }
+    },
+
+    mounted(){
+        setTimeout(() => {
+           this.getClients()
+        }, 2000)
+    }
 }
 </script>
