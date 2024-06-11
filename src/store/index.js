fc5edc8dec1b8f76996f56bde10b09d50334d46d
Eduardo Medrano <87587568+eduar-mdr@users.noreply.github.com>
2024-06-11 00:16:56 -0600

    Vuex - Store config

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,59 @@
 import { createStore } from 'vuex'
 
+import { useApiService } from '../service/apiService';
+const apiService = useApiService();
+
 export default createStore({
   state: {
+    globalVar : '',
+    GEMINI_API_KEY: '',
+
+    contextData : null,
+    contextClients: null ,
+    contextClientPurchases: null,
+    contextBooks: null,
+    contextBookCategories: null,
   },
   getters: {
+    getGlobalVar(state){
+      return state.globalVar
+    },
+    getGeminiAiApiKey(state){
+      return state.GEMINI_API_KEY
+    },
+    
+    getContextClients(state){
+
+      apiService.getClients();
+      state.contextClients = apiService.clients;
+
+      return state.contextClients
+    },
+
+    getContextClientPurchases(state){
+
+      apiService.getClientsPurchases();
+      state.contextClientPurchases = apiService.clientPurchases;
+
+      return state.contextClientPurchases
+    },
+
+    getContextBooks(state){
+
+      apiService.getBooks();
+      state.contextBooks = apiService.books;
+
+      return state.contextBooks
+    },
+
+    getContextBookCategories(state){
+     
+      apiService.getBookCategories();
+      state.contextBookCategories = apiService.bookCategories;
+
+      return state.contextBookCategories
+    }
+    
   },
   mutations: {
   },
